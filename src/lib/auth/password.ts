52b358e56e1801bc7c7ab687ec248b7c6e101b5a
Serhiy Horodetskiy <bryklin.best.1994@gmail.com>
2026-04-08 14:32:33 +0000

    Додати хешування паролів

---
+++ src/lib/auth/password.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcryptjs";
+
+const DEFAULT_SALT_ROUNDS = 12;
+
+export async function hashPassword(password: string, saltRounds = DEFAULT_SALT_ROUNDS) {
+  return bcrypt.hash(password, saltRounds);
+}
+
+export async function verifyPassword(password: string, passwordHash: string) {
+  return bcrypt.compare(password, passwordHash);
+}
+
+export { DEFAULT_SALT_ROUNDS };
